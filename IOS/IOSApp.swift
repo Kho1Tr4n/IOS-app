8e3a263325ec0fb7d4a1b3ae61e86a88c9d8e3fe
Kho1Tr4n <s3916827@rmit.edu.vn>
2023-07-28 14:54:42 +0700

    Initial Commit

---
+++ IOS/IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp.swift
+//  IOS
+//
+//  Created by Khoi Tran Minh on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
